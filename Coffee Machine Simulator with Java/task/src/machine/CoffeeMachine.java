4c11d8ad1d9b7e80454e01a727b6e4b8f48429be
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-12-28 12:49:05 -0500

    Estimate the number of servings

--- Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
+++ Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
@@ -16,7 +16,9 @@ public class CoffeeMachine {
 
         for (var option : options) System.out.println(option);
       */
-        makeCoffee();
+//        makeCoffee();
+        checkInventory();
+
     }
 
     public static void makeCoffee() {
@@ -41,4 +43,37 @@ public class CoffeeMachine {
         scanner.close();
     }
 
+    public static void checkInventory() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        int waterSupply = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int milkSupply = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        int coffeeBeansSupply = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need: ");
+        int cupsNeeded = scanner.nextInt();
+
+        int waterPerCup = 200;
+        int milkPerCup = 50;
+        int coffeeBeansPerCup = 15;
+        // Calculating the maximum number of cups the machine can make
+        int maxCups = Math.min(waterSupply / waterPerCup,
+                Math.min(milkSupply / milkPerCup, coffeeBeansSupply / coffeeBeansPerCup));
+
+        // Checking if the machine can make the exact number of cups requested
+        if (cupsNeeded > maxCups) {
+            // Not enough resources to make the desired number of cups
+            System.out.println("No, I can make only " + maxCups + " cup(s) of coffee");
+        } else if (cupsNeeded < maxCups) {
+            // More resources available than needed for the desired number of cups
+            System.out.println("Yes, I can make that amount of coffee (and even "
+                    + (maxCups - cupsNeeded) + " more than that)");
+        } else {
+            // Exact amount of resources for the desired number of cups
+            System.out.println("Yes, I can make that amount of coffee");
+        }
+        scanner.close();
+    }
 }
